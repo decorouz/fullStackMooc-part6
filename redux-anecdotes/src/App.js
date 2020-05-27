b3b245bc7a020cca2cf2174ca9aaf492447092a9
Biola <abiola2326@gmail.com>
2020-05-27 14:41:19 +0100

    updated app.js

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
-
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 
 const App = () => {
   return (
     <div>
+      <h2>Anecdotes</h2>
       <AnecdoteList />
       <AnecdoteForm />
     </div>
