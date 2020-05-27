a081ec1c4e83936cace23109145d2b08cfbbfdb5
Biola <abiola2326@gmail.com>
2020-05-27 13:32:10 +0100

    implemented voting

--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -4,7 +4,7 @@ const anecdotesAtStart = [
   'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
   'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
   'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
 ]
 
 const getId = () => (100000 * Math.random()).toFixed(0)
@@ -13,17 +13,40 @@ const asObject = (anecdote) => {
   return {
     content: anecdote,
     id: getId(),
-    votes: 0
+    votes: 0,
   }
 }
 
 const initialState = anecdotesAtStart.map(asObject)
 
 const reducer = (state = initialState, action) => {
-  console.log('state now: ', state)
+  // console.log('state now: ', state)
   console.log('action', action)
+  switch (action.type) {
+    case 'VOTE_INCREMENT':
+      const id = action.data.id
 
-  return state
+      const anecdoteToChange = state.find((a) => a.id === id)
+      const changedAnecdote = {
+        ...anecdoteToChange,
+        votes: anecdoteToChange.votes + 1,
+      }
+      return state.map((anecdote) =>
+        anecdote.id !== id ? anecdote : changedAnecdote
+      )
+
+    default:
+      return state
+  }
+}
+
+//Create an action created for voting
+
+export const voting = (id) => {
+  return {
+    type: 'VOTE_INCREMENT',
+    data: { id },
+  }
 }
 
 export default reducer
